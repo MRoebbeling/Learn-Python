6d65916e929881c713c06affb007ad27cfed14f7
MRoebbeling <46286583+MRoebbeling@users.noreply.github.com>
2020-12-26 19:20:04 +0100

    Create Lists.py
    List Variables

---
+++ Lists.py
@@ -0,0 +1,17 @@
+#Arrays or Lists
+
+cars = ['Volvo','Porsche','Ferrari']
+
+print(cars)
+
+print(cars[2])
+
+print(len(cars))
+
+print(cars[-1])
+
+print(cars[1:3]) #achtung start ist mit 0 ende mit 1
+
+cars[1] = 'VW'
+
+cars.insert(2, 'BMW')
