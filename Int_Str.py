fcec8f046612788da041ca080119d438f21cd50c
MRoebbeling <46286583+MRoebbeling@users.noreply.github.com>
2020-12-26 19:03:08 +0100

    Create Int_Str.py
    Integer and String variables in Python

---
+++ Int_Str.py
@@ -0,0 +1,32 @@
+# integer (ganze Zahlen)
+
+a = 1
+b = 2
+print(a+b)
+
+# String (Buchstaben, Zahlen Kombinationen)
+
+name1 = "horst"
+print(name1)
+
+# Stringhandling 
+
+name2 = "Hannes"
+name3 = "Wer ist der Beste? Es ist"
+print(name3+name2)
+
+# Float (ungrade Zahlen)
+
+c = 2.5
+print(a+c)
+
+# FEHLERQUELLEN
+
+fnummer = "42"
+rnummer = 42
+
+print (fnummer)
+print (rnummer)
+
+print (fnummer + 1)
+print (rnummer + 1)
